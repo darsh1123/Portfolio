ccf0a8d2113fc24b725beda8d9cc4bb9b1b5ffa1
darsh1123 <darshgautam952@gmail.com>
2024-02-16 23:17:29 +0530

    first

---
+++ images/1.js
@@ -0,0 +1,9 @@
+// <!-- hum inn template engine se dynamic content ko get kar sakte hain apni website mein -->
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.set('view engine' , 'ejs');
+
+
